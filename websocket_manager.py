7137ef486722957ae1afef617fdd75da8e915169
AegisSecure <aegissecure25@gmail.com>
2025-10-30 00:15:26 +0530

    Update websocket_manager.py

--- websocket_manager.py
+++ websocket_manager.py
@@ -1,22 +1,3 @@
-# # websocket_manager.py
-# from fastapi import WebSocket
-# active_connections = []
-
-# async def connect(websocket: WebSocket):
-#     await websocket.accept()
-#     active_connections.append(websocket)
-
-# async def disconnect(websocket: WebSocket):
-#     active_connections.remove(websocket)
-
-# async def broadcast_new_email(gmail_email: str):
-#     message = {"new_email": True, "gmail_email": gmail_email}
-#     print(f"📢 Broadcasting: {message}")
-#     for conn in active_connections:
-#         await conn.send_json(message)
-
-
-
 from fastapi import WebSocket
 from typing import List, Dict, Any
 
