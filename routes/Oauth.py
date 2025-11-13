eddf700181943ea3fac9628aea8a2f7fa63d2064
AegisSecure <aegissecure25@gmail.com>
2025-11-13 21:56:43 +0530

    Update Oauth.py

--- routes/Oauth.py
+++ routes/Oauth.py
@@ -148,7 +148,7 @@ async def google_callback(code: str, state: str = None):
             sender_email = match.group(1) if match else from_header
             snippet = msg_data.get("snippet", "")
             body = extract_body(msg_data.get("payload", {}))
-            combined_text = f"{subject} {snippet}"
+            combined_text = f"{subject} {body}"
 
             # print("This is new Email"+body);
             spam_result = await get_spam_prediction(combined_text)
